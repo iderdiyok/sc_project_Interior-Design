85ccf302c430f9a3e397aac8b550171ff1180c7c
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-02-01 13:06:19 +0100

    fixed nav-sticky, added js;

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function formInfo() {
+    let name = document.getElementById("name").value
+    let email = document.getElementById("email").value
+    let message = document.getElementById("message").value
+
+    console.log("Name: " + name + "\nE-Mail: " + email + "\nMessage: " + message)
+}
